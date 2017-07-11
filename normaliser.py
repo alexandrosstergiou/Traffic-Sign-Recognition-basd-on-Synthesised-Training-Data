c442b9e50f2267006f201a71aaf61480ea9680d1
alexandrosstergiou <alexstergiou5@gmail.com>
2017-07-11 20:59:26 +0100

    Adding blu

--- normaliser.py
+++ normaliser.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from PIL import Image
 import sys
+import cv2
 
 def load_paths(directory):
     paths = []
@@ -10,22 +11,60 @@ def load_paths(directory):
             paths.append(directory+'/'+files)
     return paths
 
+def blur(image):
+    kernel = np.ones((5,5),np.float32)/25
+    dst = cv2.filter2D(image,-1,kernel)
+    #blur = cv2.bilateralFilter(image,9,75,75)
+    return dst
 
-
-def resize_and_save(path,size):
-
-    head,tail = path.split('.')
-    output = head+".jpg"
+def resize_and_save(path,size,directory):
+    
     try:
         img = Image.open(path)
         img = img.resize(size)
-        os.remove(path)
-        img.save(output, "JPEG")
+        im = np.uint8(img)
+        blurred = blur(im)
+        img = Image.fromarray(blurred)
+        img.save(directory, "JPEG")
     except IOError:
         print "cannot create thumbnail for '%s'" % infile
+
+
+directory = 'val_set_blurred'
+if (not os.path.exists('val_set_blurred')):
+    for sign in load_paths("Traffic_Signs_Templates/Images"):
+        head,tail = sign.split('.')
+        name = []
+        name = head.split('/')
+        os.makedirs('val_set_blurred/'+name[-1])
+
+
+
 
 paths = load_paths("val_set")
 for path in paths:
     temp = load_paths(path)
     for p in temp:
-        resize_and_save(p,(48,48))
+        elements = p.split('/')
+        d = 'val_set_blurred/'+elements[-2]+"/"+elements[-1]
+        resize_and_save(p,(48,48), d)
+
+
+
+directory = 'SGTSD/Images_blurred/'
+if (not os.path.exists('SGTSD/Images_blurred/')):
+    for sign in load_paths("Traffic_Signs_Templates/Images"):
+        head,tail = sign.split('.')
+        name = []
+        name = head.split('/')
+        os.makedirs('SGTSD/Images_blurred/'+name[-1])
+
+
+paths = load_paths("SGTSD/Images/")
+for path in paths:
+    temp = load_paths(path)
+    for p in temp:
+        elements = p.split('/')
+        d = 'SGTSD/Images_blurred/'+elements[-2]+"/"+elements[-1]
+        resize_and_save(p,(48,48), d)
+
